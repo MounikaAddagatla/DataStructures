97a15bfa49783637625472e418ecbd39b7318e3d
MounikaAddagatla <89588852+MounikaAddagatla@users.noreply.github.com>
2021-10-07 20:18:23 +0530

    Added: Linked List operations

---
+++ DataStructures/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This Is Datastructure  programs");
+            LinkedListOperations linkedList = new LinkedListOperations();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+            linkedList.RemoveLastNode();
+            Console.WriteLine("After deleting last node");
+            linkedList.Display();
+            Console.WriteLine("Searching");
+            linkedList.Search(30);
+            linkedList.Display();
+        }
+    }
+}
