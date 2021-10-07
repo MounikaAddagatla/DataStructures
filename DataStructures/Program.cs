78b456bb90c3d5e5289f205a78b65ec263852caf
MounikaAddagatla <89588852+MounikaAddagatla@users.noreply.github.com>
2021-10-07 20:28:25 +0530

    Added: Stack methods to UC2

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -7,17 +7,13 @@ namespace DataStructures
         static void Main(string[] args)
         {
             Console.WriteLine("This Is Datastructure  programs");
-            LinkedListOperations linkedList = new LinkedListOperations();
-            linkedList.Add(56);
-            linkedList.Add(30);
-            linkedList.Add(70);
-            linkedList.Display();
-            linkedList.RemoveLastNode();
-            Console.WriteLine("After deleting last node");
-            linkedList.Display();
-            Console.WriteLine("Searching");
-            linkedList.Search(30);
-            linkedList.Display();
+            StackOperations stack = new StackOperations();
+            stack.push(56);
+            stack.push(30);
+            stack.push(70);
+            stack.Peek();
+            stack.Pop();
+            stack.Display();
         }
-    }
+    }    
 }
